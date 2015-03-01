b1b558a23dcd2b416e144d43fb9d38cef2e0d1f3
Johny-kann <johny.manasee@gmail.com>
2015-02-28 22:13:50 -0500

    Final result
    The project is complete

--- src/main/java/com/computer_graphics/constants/files/FileConstants.java
+++ src/main/java/com/computer_graphics/constants/files/FileConstants.java
@@ -2,12 +2,12 @@ package com.computer_graphics.constants.files;
 
 public class FileConstants {
 
-	public static final String WRAP_SOURCE_IMAGE = "/Wraping_Images/Rahman.jpg";
-	public static final String WRAP_DESTINATION_IMAGE_TEMPLATE = "/Wraping_Images/Rahman.jpg";
+	public static final String WRAP_SOURCE_IMAGE = "/Wraping_Images/em-font-size.png";
+	public static final String WRAP_DESTINATION_IMAGE_TEMPLATE = "/Wraping_Images/em-font-size.png";
 	public static final String LOG = "Log/docs.log";
 	
 	public static final String BLEND_SOURCE_IMAGE = "/Wraping_Images/Boy.jpg";
-	public static final String BLEND_DESTINATION_IMAGE = "/Wraping_Images/Baby-Crop.jpg";
+	public static final String BLEND_DESTINATION_IMAGE = "/Wraping_Images/Girl.jpg";
 	
 	public static final String FXML_WRAP = "/WrapFXML.fxml";
 	public static final String FXML_BLEND = "/BlendFXML.fxml";
